009ec2c4647f95237b5bdb8896657ed6478d66ed
Koharu Saeki <koharu0929alexandros@gmail.com>
2025-06-08 23:59:07 +0900

    limitを指定できるように

--- 02_evenFibonacciNumbers.py
+++ 02_evenFibonacciNumbers.py
@@ -1,8 +1,9 @@
-def getSumOfEvenFibonacciNumbers():
+# limit以下の偶数Fibonacci数の和を求める関数
+def getSumOfEvenFibonacciNumbers(limit):
     sum = 0
     i = 1
 
-    while(getFibonacci(i) < 4000000):
+    while(getFibonacci(i) < limit):
         i += 1
         if getFibonacci(i) % 2 == 0:
             sum += getFibonacci(i)
@@ -20,4 +21,4 @@ def getFibonacci(n):
     else:
         return getFibonacci(n-1) + getFibonacci(n-2)
 
-print(getSumOfEvenFibonacciNumbers()) # 4613732
+print(getSumOfEvenFibonacciNumbers(4000000)) # 4613732
