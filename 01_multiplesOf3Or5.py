2a0f48be76bf24ce2e5102a233cc12d064f7bb06
Koharu Saeki <koharu0929alexandros@gmail.com>
2025-06-08 23:37:45 +0900

    add comments

--- 01_multiplesOf3Or5.py
+++ 01_multiplesOf3Or5.py
@@ -2,4 +2,4 @@ def getMultiplesOfSum(limit):
     multiples = [x for x in range(limit) if x % 3 == 0 or x % 5 == 0]
     return sum(multiples)
 
-print(getMultiplesOfSum(1000))
+print(getMultiplesOfSum(1000)) # Output: 233168
